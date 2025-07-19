b5f9b519ecf9d4979b679501f0771f18532e4e42
olamide <olamoyegunolamidesammy@gmail.com>
2025-07-19 02:41:28 +0100

    Add command to automatically create a superuser using environment variables

--- apps/management/commands/createsuperuser_auto.py.py
+++ apps/management/commands/createsuperuser_auto.py.py
@@ -2,7 +2,6 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
 import os
 import logging
-from django.utils.crypto import get_random_string
 from django.core.management import CommandError
 
 
