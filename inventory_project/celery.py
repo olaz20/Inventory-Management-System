ca7c57e3e602dfd69430cd22142154507a6569d8
olamide <olamoyegunolamidesammy@gmail.com>
2025-07-22 20:49:10 +0100

    Configure Celery settings for different environments; set up broker URL and serialization in development and production settings.

--- inventory_project/celery.py
+++ inventory_project/celery.py
@@ -2,7 +2,17 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 from dotenv import load_dotenv
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_project.settings.base')
+
+ENV = os.getenv("ENV", "development")
+if ENV == "production":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_project.settings.production')
+elif ENV == "testing":
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_project.settings.testing')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventory_project.settings.development')
+
+
+
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env'))
 
 import django
